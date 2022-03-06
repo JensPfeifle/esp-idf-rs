1440cfe37a9b4f70fed496f40baf0296c1ae0885
Jens Pfeifle <jens@pfeifle.tech>
2022-03-06 20:12:16 +0100

    Fix rotation in epd-gfx.

--- epd-gfx/src/lib.rs
+++ epd-gfx/src/lib.rs
@@ -1,5 +1,6 @@
-const WIDTH: u32 = 540;
-const HEIGHT: u32 = 960;
+const WIDTH: u32 = 960;
+const HEIGHT: u32 = 540;
+const ROTATED: bool = true;
 
 /// Split a framebuffer byte into two pixels of 4 bits each.
 /// ```
@@ -14,7 +15,13 @@ pub fn split_byte(byte: u8) -> (u8, u8) {
     return (left, right);
 }
 
-pub fn draw_pixel(fb: &mut [u8], x: usize, y: usize, color: u8) {
+pub fn draw_pixel(fb: &mut [u8], mut x: u32, mut y: u32, color: u8) {
+    if ROTATED {
+        let tmp = x;
+        x = y;
+        y = tmp;
+        x = WIDTH - x - 1;
+    }
     if x as u32 >= WIDTH {
         return;
     }
@@ -22,7 +29,7 @@ pub fn draw_pixel(fb: &mut [u8], x: usize, y: usize, color: u8) {
         return;
     }
 
-    let fb_index = y * WIDTH as usize / 2 + x / 2;
+    let fb_index = (y * WIDTH as u32 / 2 + x / 2) as usize;
     let mut fb_byte = fb[fb_index];
     if x % 2 == 0 {
         fb_byte = (fb_byte & 0xF0) | (color >> 4);
@@ -36,21 +43,21 @@ pub fn set_all(fb: &mut [u8], color: u8) {
     fb.iter_mut().for_each(|x| *x = color);
 }
 
-pub fn draw_hline(fb: &mut [u8], x: usize, y: usize, length: usize, color: u8) {
+pub fn draw_hline(fb: &mut [u8], x: u32, y: u32, length: u32, color: u8) {
     for i in 0..length {
         let xx = x + i;
         draw_pixel(fb, xx, y, color);
     }
 }
 
-pub fn draw_vline(fb: &mut [u8], x: usize, y: usize, length: usize, color: u8) {
+pub fn draw_vline(fb: &mut [u8], x: u32, y: u32, length: u32, color: u8) {
     for i in 0..length {
         let yy = y + i;
         draw_pixel(fb, x, yy, color);
     }
 }
 
-pub fn fill_rect(fb: &mut [u8], x: usize, y: usize, w: usize, h: usize, color: u8) {
+pub fn fill_rect(fb: &mut [u8], x: u32, y: u32, w: u32, h: u32, color: u8) {
     for i in y..y + h {
         draw_hline(fb, x, i, w, color);
     }
