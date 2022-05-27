79d33fc0199e7a62291afa1d1276e9a105a758ca
Jens Pfeifle <jens@pfeifle.tech>
2022-05-27 21:57:10 +0200

    [wip] trying to fix build...

--- pio/build.rs
+++ pio/build.rs
@@ -1,20 +1,35 @@
 use std::env;
 use std::path::{Path, PathBuf};
 
-fn main() {
+use embuild::{
+    self, bingen,
+    build::{CfgArgs, LinkArgs},
+    cargo, symgen,
+};
+
+fn main() -> anyhow::Result<()> {
     let target = env::var("TARGET").unwrap();
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
 
     let header = "./components/epdiy/include/epd_highlevel.h";
     let out = out_dir.join("epd_highlevel.rs");
     run_bindgen(&target, header, &out);
+
+    // Necessary because of this issue: https://github.com/rust-lang/cargo/issues/9641
+    LinkArgs::output_propagated("ESP_IDF")?;
+    let cfg = CfgArgs::try_from_env("ESP_IDF")?;
+    cfg.output();
+
+    Ok(())
 }
 
 fn run_bindgen(target: &str, header: &str, out: &Path) {
     let mut builder = bindgen::Builder::default();
     builder = builder.header(header).clang_args([
         "-I./.pio/build/debug/config/", // sdkconfig.h
-        "-I/root/.platformio/packages/framework-espidf/components/xtensa/include",
+        "-I/opt/esp/idf/components/esp_common/include",
+        "-I/opt/esp/idf/components/xtensa/include",
+        "-I/opt/esp/tools/xtensa-esp32-elf/esp-2021r2-patch3-8.4.0/xtensa-esp32-elf/xtensa-esp32-elf/include/",
     ]);
     match target {
         "riscv32imc-esp-espidf" => {
